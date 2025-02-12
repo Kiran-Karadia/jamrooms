aada6144ec7c5ce1ecef264d574cf113c5edc83d
Kiran Karadia <kiran_karadia@hotmail.com>
2025-02-12 17:52:12 +0000

    Move SpotifyTokenRepository to its own file

---
+++ backend/jamrooms/src/main/kotlin/com/kiran/repositories/SpotifyTokenRepository.kt
@@ -0,0 +1,12 @@
+package com.kiran.repositories
+
+import com.kiran.entities.SpotifyToken
+import io.micronaut.data.model.query.builder.sql.Dialect
+import io.micronaut.data.r2dbc.annotation.R2dbcRepository
+import io.micronaut.data.repository.kotlin.CoroutineCrudRepository
+
+@R2dbcRepository(dialect = Dialect.POSTGRES)
+interface SpotifyTokenRepository: CoroutineCrudRepository<SpotifyToken, Long> {
+    suspend fun findByUserId(userId: String = "KwKiran"): SpotifyToken
+}
+
