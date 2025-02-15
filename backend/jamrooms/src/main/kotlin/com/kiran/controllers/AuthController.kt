edb530691b0e20e80f69ef590b1b8921113b6c9c
Kiran Karadia <kiran_karadia@hotmail.com>
2025-02-15 23:00:28 +0000

    Remove endpoint that was just for testing

--- backend/jamrooms/src/main/kotlin/com/kiran/controllers/AuthController.kt
+++ backend/jamrooms/src/main/kotlin/com/kiran/controllers/AuthController.kt
@@ -1,6 +1,5 @@
 package com.kiran.controllers
 
-import com.kiran.entities.SpotifyToken
 import com.kiran.services.AuthService
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
@@ -24,13 +23,5 @@ class AuthController(
     ): HttpResponse<*> {
         val tokenResponse = authService.exchangeAuthCodeForToken(code)
         return HttpResponse.ok(tokenResponse)
-    }
-
-    // Just for testing
-    @Get("/allTokens")
-    suspend fun getAllTokens(): HttpResponse<List<SpotifyToken>> {
-        return HttpResponse.ok(
-            authService.getAllTokens()
-        )
     }
 }
