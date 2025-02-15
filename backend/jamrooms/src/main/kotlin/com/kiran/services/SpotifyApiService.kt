0bddc45534c49dc7d21e79b29cd53b0fd54d213d
Kiran Karadia <kiran_karadia@hotmail.com>
2025-02-15 22:59:10 +0000

    Migrate methods to SpotifyApiService

--- backend/jamrooms/src/main/kotlin/com/kiran/services/SpotifyApiService.kt
+++ backend/jamrooms/src/main/kotlin/com/kiran/services/SpotifyApiService.kt
@@ -1,5 +1,8 @@
 package com.kiran.services
 
+import com.kiran.dtos.responses.spotifyapi.AvailableDevicesResponse
+import com.kiran.dtos.responses.spotifyapi.CurrentUserProfileResponse
+import com.kiran.dtos.responses.spotifyapi.PlaybackStateResponse
 import com.kiran.httpclients.SpotifyApiClient
 import jakarta.inject.Singleton
 
@@ -7,5 +10,26 @@ import jakarta.inject.Singleton
 class SpotifyApiService(
     private val spotifyApiClient: SpotifyApiClient,
 ) {
+    suspend fun getUserProfile(): CurrentUserProfileResponse {
+        val currentUserProfileResponse = spotifyApiClient.getCurrentUserProfile()
+        return currentUserProfileResponse
+    }
 
+    suspend fun getAvailableDevices(): AvailableDevicesResponse {
+        val availableDevicesResponse = spotifyApiClient.getAvailableDevices()
+        return availableDevicesResponse
+    }
+
+    suspend fun getPlaybackState(): PlaybackStateResponse {
+        val playbackStateResponse = spotifyApiClient.getPlaybackState()
+        return playbackStateResponse
+    }
+
+    suspend fun togglePlayback() {
+        val playbackStateResponse = spotifyApiClient.getPlaybackState()
+        when (playbackStateResponse.isPlaying) {
+            true -> spotifyApiClient.pausePlayback()
+            false -> spotifyApiClient.startOrResumePlayback()
+        }
+    }
 }
