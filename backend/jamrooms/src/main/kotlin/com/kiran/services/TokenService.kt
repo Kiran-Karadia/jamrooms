92105776f4196f17d5460aeea04556a5ca72bf02
Kiran Karadia <kiran_karadia@hotmail.com>
2025-02-16 01:09:02 +0000

    Add method to check if a given userId is authenticated. No refresh token logic yet. Also throw exception if no token is found

--- backend/jamrooms/src/main/kotlin/com/kiran/services/TokenService.kt
+++ backend/jamrooms/src/main/kotlin/com/kiran/services/TokenService.kt
@@ -3,17 +3,29 @@ package com.kiran.services
 import com.kiran.entities.SpotifyToken
 import com.kiran.repositories.SpotifyTokenRepository
 import jakarta.inject.Singleton
+import org.slf4j.LoggerFactory
+import java.time.Instant
 
 @Singleton
 class TokenService(
     private val spotifyTokenRepository: SpotifyTokenRepository
 ) {
+
+    private val logger = LoggerFactory.getLogger(this.javaClass.simpleName)
+
     suspend fun getAuthHeaderValue(): String {
-        val spotifyToken = spotifyTokenRepository.findByUserId()
+        val spotifyToken = spotifyTokenRepository.findByUserId() ?: throw Exception("Token not found")
         return "${spotifyToken.tokenType} ${spotifyToken.accessToken}"
     }
 
     suspend fun saveSpotifyToken(spotifyToken: SpotifyToken): SpotifyToken {
         return spotifyTokenRepository.save(spotifyToken)
+    }
+
+    suspend fun isAuthenticated(userId: String): Boolean {
+        val spotifyToken = spotifyTokenRepository.findByUserId(userId) ?: return false
+
+        return Instant.now().isBefore(spotifyToken.updatedAt!!.plusSeconds(spotifyToken.expiresIn.toLong()))
+        //TODO: Refresh logic still needed...
     }
 }
